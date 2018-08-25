d8d6b9ea55044ac627e1890321dc26653187b6f2
normanrs <normanrs@live.com>
2018-08-25 17:07:23 -0600

    Working offset

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -11,4 +11,26 @@ class Enigma
                     " ", ".", "," ]
   end
 
+
+  def encrypt
+  end
+
+  def date_squared
+    day = Date.today.day.to_s.rjust(2, "0")
+    month = Date.today.month.to_s.rjust(2, "0")
+    date_int = (Date.today.year.to_s + month + day).to_i
+    date_int ** 2
+  end
+
+  def offset
+    offset_array = []
+    string = date_squared.to_s
+    strip = string[-4..-1]
+    characters = strip.chars
+    characters.each do |x|
+      offset_array << x.to_i
+    end
+    offset_array
+  end
+
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -13,5 +13,19 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, e
   end
 
+  def test_it_square_todays_date
+    e = Enigma.new
+    actual = e.date_squared
+    expected = 407265697680625
+    assert_equal expected, actual
+  end
+
+  def test_it_creates_the_offset
+    e = Enigma.new
+    actual = e.offset
+    expected = [0, 6, 2, 5]
+    assert_equal expected, actual
+  end
+
 
 end
